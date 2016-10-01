41fd06edc7c16a673bd5d1d1b9f13c267f20b4a1
Francis Erik Razote <francis.razote@gmail.com>
2016-10-01 17:29:29 +0800

    Refactor Inventory Provider

--- app/src/main/java/com/example/android/inventory/data/InventoryProvider.java
+++ app/src/main/java/com/example/android/inventory/data/InventoryProvider.java
@@ -13,9 +13,9 @@ import com.example.android.inventory.data.InventoryContract.InventoryEntry;
 
 public class InventoryProvider extends ContentProvider {
 
-    private static final int PETS = 100;
+    private static final int INVENTORY = 100;
 
-    private static final int PET_ID = 101;
+    private static final int INVENTORY_ID = 101;
 
     public static final String LOG_TAG = InventoryProvider.class.getSimpleName();
 
@@ -24,8 +24,8 @@ public class InventoryProvider extends ContentProvider {
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
-        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY, PETS);
-        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY + "/#", PET_ID);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY, INVENTORY);
+        sUriMatcher.addURI(InventoryContract.CONTENT_AUTHORITY, InventoryContract.PATH_INVENTORY + "/#", INVENTORY_ID);
     }
 
     @Override
@@ -37,37 +37,22 @@ public class InventoryProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        // Get readable database
+
         SQLiteDatabase database = mDbHelper.getReadableDatabase();
 
-        // This cursor will hold the result of the query
         Cursor cursor;
 
-        // Figure out if the URI matcher can match the URI to a specific code
         int match = sUriMatcher.match(uri);
         switch (match) {
-            case PETS:
-                // For the PETS code, query the pets table directly with the given
-                // projection, selection, selection arguments, and sort order. The cursor
-                // could contain multiple rows of the pets table.
+            case INVENTORY:
                 cursor = database.query(InventoryContract.InventoryEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
 
                 break;
-            case PET_ID:
-                // For the PET_ID code, extract out the ID from the URI.
-                // For an example URI such as "content://com.example.android.pets/pets/3",
-                // the selection will be "_id=?" and the selection argument will be a
-                // String array containing the actual ID of 3 in this case.
-                //
-                // For every "?" in the selection, we need to have an element in the selection
-                // arguments that will fill in the "?". Since we have 1 question mark in the
-                // selection, we have 1 String in the selection arguments' String array.
+            case INVENTORY_ID:
                 selection = InventoryEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
 
-                // This will perform a query on the pets table where the _id equals 3 to return a
-                // Cursor containing that row of the table.
                 cursor = database.query(InventoryContract.InventoryEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
                 break;
@@ -84,29 +69,28 @@ public class InventoryProvider extends ContentProvider {
     public Uri insert(Uri uri, ContentValues contentValues) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
-            case PETS:
-                return insertPet(uri, contentValues);
+            case INVENTORY:
+                return insertInventory(uri, contentValues);
             default:
                 throw new IllegalArgumentException("Insertion is not supported for " + uri);
         }
     }
 
-    private Uri insertPet(Uri uri, ContentValues values) {
+    private Uri insertInventory(Uri uri, ContentValues values) {
         // Check that the name is not null
         String name = values.getAsString(InventoryContract.InventoryEntry.COLUMN_INVENTORY_NAME);
         if (name == null) {
-            throw new IllegalArgumentException("Pet requires a name");
+            throw new IllegalArgumentException("Inventory requires a name");
         }
 
-        Integer gender = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
-        if (gender == null || !InventoryEntry.isValidGender(gender)) {
-            throw new IllegalArgumentException("Pet requires valid gender");
+        Integer quantity = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+        if (quantity == null || quantity < 0) {
+            throw new IllegalArgumentException("Inventory requires valid quantity");
         }
 
-        // If the weight is provided, check that it's greater than or equal to 0 kg
-        Integer weight = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_PRICE);
-        if (weight != null && weight < 0) {
-            throw new IllegalArgumentException("Pet requires valid weight");
+        Integer price = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_PRICE);
+        if (price == null || price < 0) {
+            throw new IllegalArgumentException("Inventory requires valid price");
         }
 
         // Get writeable database
@@ -131,50 +115,42 @@ public class InventoryProvider extends ContentProvider {
     public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
-            case PETS:
-                return updatePet(uri, contentValues, selection, selectionArgs);
-            case PET_ID:
-                // For the PET_ID code, extract out the ID from the URI,
+            case INVENTORY:
+                return updateInventory(uri, contentValues, selection, selectionArgs);
+            case INVENTORY_ID:
+                // For the INVENTORY_ID code, extract out the ID from the URI,
                 // so we know which row to update. Selection will be "_id=?" and selection
                 // arguments will be a String array containing the actual ID.
                 selection = InventoryEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                return updatePet(uri, contentValues, selection, selectionArgs);
+                return updateInventory(uri, contentValues, selection, selectionArgs);
             default:
                 throw new IllegalArgumentException("Update is not supported for " + uri);
         }
     }
 
-    private int updatePet(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        // If the {@link InventoryEntry#COLUMN_INVENTORY_NAME} key is present,
-        // check that the name value is not null.
+    private int updateInventory(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         if (values.containsKey(InventoryEntry.COLUMN_INVENTORY_NAME)) {
             String name = values.getAsString(InventoryEntry.COLUMN_INVENTORY_NAME);
             if (name == null) {
-                throw new IllegalArgumentException("Pet requires a name");
+                throw new IllegalArgumentException("Inventory requires a name");
             }
         }
 
-        // If the {@link InventoryEntry#COLUMN_INVENTORY_QUANTITY} key is present,
-        // check that the gender value is valid.
         if (values.containsKey(InventoryEntry.COLUMN_INVENTORY_QUANTITY)) {
-            Integer gender = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
-            if (gender == null || !InventoryEntry.isValidGender(gender)) {
-                throw new IllegalArgumentException("Pet requires valid gender");
+            Integer quantity = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_QUANTITY);
+            if (quantity == null || quantity < 0) {
+                throw new IllegalArgumentException("Inventory requires valid quantity");
             }
         }
 
-        // If the {@link InventoryEntry#COLUMN_INVENTORY_PRICE} key is present,
-        // check that the weight value is valid.
         if (values.containsKey(InventoryContract.InventoryEntry.COLUMN_INVENTORY_PRICE)) {
             // Check that the weight is greater than or equal to 0 kg
-            Integer weight = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_PRICE);
-            if (weight != null && weight < 0) {
-                throw new IllegalArgumentException("Pet requires valid weight");
+            Integer price = values.getAsInteger(InventoryEntry.COLUMN_INVENTORY_PRICE);
+            if (price == null || price < 0) {
+                throw new IllegalArgumentException("Inventory requires valid price");
             }
         }
-
-        // No need to check the breed, any value is valid (including null).
 
         // If there are no values to update, then don't try to update the database
         if (values.size() == 0) {
@@ -207,7 +183,7 @@ public class InventoryProvider extends ContentProvider {
 
         final int match = sUriMatcher.match(uri);
         switch (match) {
-            case PETS:
+            case INVENTORY:
                 // Delete all rows that match the selection and selection args
                 rowsDeleted = database.delete(InventoryEntry.TABLE_NAME, selection, selectionArgs);
                 // If 1 or more rows were deleted, then notify all listeners that the data at the
@@ -216,7 +192,7 @@ public class InventoryProvider extends ContentProvider {
                     getContext().getContentResolver().notifyChange(uri, null);
                 }
                 return rowsDeleted;
-            case PET_ID:
+            case INVENTORY_ID:
                 // Delete a single row given by the ID in the URI
                 selection = InventoryContract.InventoryEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };

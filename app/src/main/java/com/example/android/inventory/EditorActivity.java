aafa06728759982f28c47f8cbb9f89b1215b9e25
Francis Erik Razote <francis.razote@gmail.com>
2016-10-01 20:11:32 +0800

    Add functionality for the camera button

--- app/src/main/java/com/example/android/inventory/EditorActivity.java
+++ app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -10,32 +10,46 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.inventory.data.InventoryContract;
 import com.example.android.inventory.data.InventoryContract.InventoryEntry;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class EditorActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
+
+    private ImageView mImageView;
 
     private EditText mNameEditText;
 
     private EditText mBreedEditText;
 
-    private EditText mGenderEditText;
+    private EditText mQuantityEditText;
 
-    private EditText mWeightEditText;
+    private EditText mPriceEditText;
+
+    private String mCurrentPhotoPath;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
 
     /**
      * Gender of the pet. The possible values are:
@@ -77,27 +91,79 @@ public class EditorActivity extends AppCompatActivity implements
             getLoaderManager().initLoader(EXISTING_INVENTORY_LOADER, null, this);
         }
 
+        mImageView = (ImageView) findViewById(R.id.image);
+
         // Find all relevant views that we will need to read user input from
         mNameEditText = (EditText) findViewById(R.id.edit_inventory_name);
         mBreedEditText = (EditText) findViewById(R.id.edit_inventory_breed);
-        mGenderEditText = (EditText) findViewById(R.id.edit_inventory_quantity);
-        mWeightEditText = (EditText) findViewById(R.id.edit_pet_weight);
+        mQuantityEditText = (EditText) findViewById(R.id.edit_inventory_quantity);
+        mPriceEditText = (EditText) findViewById(R.id.edit_inventory_price);
 
         // Setup OnTouchListeners on all the input fields, so we can determine if the user
         // has touched or modified them. This will let us know if there are unsaved changes
         // or not, if the user tries to leave the editor without saving.
         mNameEditText.setOnTouchListener(mTouchListener);
         mBreedEditText.setOnTouchListener(mTouchListener);
-        mWeightEditText.setOnTouchListener(mTouchListener);
+        mQuantityEditText.setOnTouchListener(mTouchListener);
+        mPriceEditText.setOnTouchListener(mTouchListener);
+
+        Button cameraButton = (Button) findViewById(R.id.camera_button);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                dispatchTakePictureIntent();
+            }
+        });
 
     }
 
-    private void savePet() {
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.inventory",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//
+//            }
+//        }
+//    }
+//
+//    private File createImageFile() throws IOException {
+//        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+//
+//        // Save a file: path for use with ACTION_VIEW intents
+//        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+//
+//        return image;
+//    }
+
+    private void saveInventory() {
         // Read from input fields
         // Use trim to eliminate leading or trailing white space
         String nameString = mNameEditText.getText().toString().trim();
         String breedString = mBreedEditText.getText().toString().trim();
-        String weightString = mWeightEditText.getText().toString().trim();
+        String weightString = mPriceEditText.getText().toString().trim();
 
         if (mCurrentInventoryUri == null &&
                 TextUtils.isEmpty(nameString) && TextUtils.isEmpty(breedString) &&
@@ -171,7 +237,7 @@ public class EditorActivity extends AppCompatActivity implements
         switch (item.getItemId()) {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
-                savePet();
+                saveInventory();
                 finish();
                 return true;
             // Respond to a click on the "Delete" menu option
@@ -348,7 +414,7 @@ public class EditorActivity extends AppCompatActivity implements
             // Update the views on the screen with the values from the database
             mNameEditText.setText(name);
             mBreedEditText.setText(breed);
-            mWeightEditText.setText(Integer.toString(weight));
+            mPriceEditText.setText(Integer.toString(weight));
         }
     }
 
@@ -357,7 +423,7 @@ public class EditorActivity extends AppCompatActivity implements
         // If the loader is invalidated, clear out all the data from the input fields.
         mNameEditText.setText("");
         mBreedEditText.setText("");
-        mGenderEditText.setText("");
-        mWeightEditText.setText("");
+        mQuantityEditText.setText("");
+        mPriceEditText.setText("");
     }
 }
